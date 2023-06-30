b8c846ed3552fa358cda262f169c3764105828d3
DedSec-Dalen <71486580+DedSec-Dalen@users.noreply.github.com>
2023-06-30 16:06:01 +0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,28 +1,31 @@
+package basics;
+
 public class Student {
+
     private String name;
-    private String surname;
-    private String address;
     private int id;
+    private int year;
 
-    public Student(String name, String surname, String address, int id) {
+    public  Student(String name, int id) {
         this.name = name;
-        this.surname = surname;
-        this.address = address;
         this.id = id;
+        this.year = 2023;
     }
 
-    public String toString() {
-        return "Name: " + name + "\nSurname: " + surname + "\nAddress: " + address + "\nID: " + id;
+    public String getName() {
+        return name;
     }
 
-    public static void main(String[] args) {
-        Student student1 = new Student("Алиса", "Смит", "Коктем-2", 24);
-        Student student2 = new Student("Азамат", "Алишерович", "Навои-31", 19);
-        Student student3 = new Student("Кристина", "Тяпкина", "Набережная-244", 25);
-
-        System.out.println(student1.toString());
-        System.out.println(student2.toString());
-        System.out.println(student3.toString());
-    }
+    public int getId() {
+        return id;
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void increase() {
+        year++;
+    }
+
+}
